f863629762469a77b58d7ed720a9e82cb80ef2b1
Mluara <martins.luara@escola.pr.gov.br>
2025-10-08 11:58:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const botoes = document.querySelectorAll(".btn-proximo");
-    const passos = document.querySelectorAll(".passo");
+const avanca = document.querySelectorAll('.btn-proximo');
 
-    botoes.forEach(botao => {
-        botao.addEventListener("click", () => {
-            const proximo = botão.getAttribute("data-proximo");
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-            passos.forEach(passo => passo.classList.remove("ativo"));
-
-            const próximoPasso = document.getElementById(`passo-${proximo}`);
-            if (proximoPasso) {
-                próximoPasso.classList.add("ativo");
-            }
-        });
-    });
-});
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
